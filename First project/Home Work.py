4e99d457b8d4a0aaa0a43358465661c336ed88be
unknown <krasnov.dmytro@gmail.com>
2022-05-25 21:08:26 +0300

    Hide .idea dir from git

--- First project/Home Work.py
+++ First project/Home Work.py
@@ -1,1 +1,27 @@
 
+print('PyCharm')
+print(3)
+print(3,4,'something',4.25)
+
+#password = input("Please give a password: ")
+#print("Your password: ", password)
+
+
+# Dynamic Typing
+a = 5
+print(type(a), id(a))
+a = "five"
+print(type(a), id(a))
+
+b = a
+print(type(b))
+
+# Strong Typing
+
+print(3 == int("3"))
+print(type(3), type(3.5))
+print(int(3.5))
+print(3+3.5)
+
+#Error
+#a=3/0
